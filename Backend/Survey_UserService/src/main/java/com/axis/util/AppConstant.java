6bd834ea005397d03806aff5507ebd2dbc868c94
Budidha-Sravani <116790560+Budidha-Sravani@users.noreply.github.com>
2023-01-18 11:36:51 +0530

    Delete Backend directory

--- Backend/Survey_UserService/src/main/java/com/axis/util/AppConstant.java
+++
@@ -1,9 +0,0 @@
-package com.axis.util;
-
-public interface AppConstant {
-	
-	public static final String INVALID_MSG="Invalid id";
-	public static final String USER_ERR="user name cannot be empty";
-	public static final String EMAIL_ERR="user email cannot be empty"; 
-
-}
